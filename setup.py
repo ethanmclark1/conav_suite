11c78192cd661186e6b8b27f2c45eec188eab8d5
Ethan <eclark715@gmail.com>
2023-07-25 15:21:49 -0700

    Fixed imports

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="Signal8",
-    version="5.0.3",
+    version="5.0.4",
     packages=find_packages(),
     author="Ethan Clark",
     author_email="eclark715@gmail.com.com",
