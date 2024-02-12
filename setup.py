cbf5ae6e899f074fe9f288ed22807ed6af0f90e2
Ethan <eclark715@gmail.com>
2024-02-11 21:42:51 -0700

    Updated pypi package number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="Signal8",
-    version="5.0.7",
+    version="5.0.8",
     packages=find_packages(),
     author="Ethan Clark",
     author_email="eclark715@gmail.com.com",
