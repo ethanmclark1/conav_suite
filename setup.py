f4c89f4827ba3157f7a04d80742182b7b6aba80f
Ethan Clark <eclark715@gmail.com>
2025-03-22 13:44:36 -0700

    update setup

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="Signal8",
-    version="5.1.2",
+    name="conav_suite",
+    version="0.0.2",
     packages=find_packages(),
     author="Ethan Clark",
     author_email="eclark715@gmail.com.com",
-    description="A multi-agent environment inspired by the Lewis Signaling Game, featuring eight unique problem configurations with both static and dynamic obstacles.",
+    description="A problem suite focused on evaluating the ability of autonomous agents to develop emergent communication strategies in cooperative navigation tasks",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url="https://github.com/ethanmclark1/signal8",
+    url="https://github.com/ethanmclark1/conav_suite",
     classifiers=[
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
